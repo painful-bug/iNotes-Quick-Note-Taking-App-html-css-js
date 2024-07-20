cc3d72d0c4ae2d71aae3775b6f1e6446cd4c354f
Aishikboy109 <ujanaishik109@gmail.com>
2024-07-21 01:10:22 +0530

    barebones modals are working

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const toggleSearch = () => {
+  const searchForm = document.querySelector(".search-form");
+  const searchButton = document.querySelector(".search-button");
+  const searchInput = document.querySelector(".search-input");
+
+  searchButton.addEventListener("click", () => {
+    searchForm.classList.toggle("active-search");
+  });
+
+  searchInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      searchInput.value = "";
+      searchForm.classList.remove("active-search");
+    }
+  });
+};
+
+function toggleBlur() {
+  const main = document.querySelector(".main");
+  main.classList.toggle("active-blur");
+  const modal = document.querySelector(".modal");
+  modal.classList.toggle("active-modal");
+}
+toggleSearch();
+
+toggleBlur();
